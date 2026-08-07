cf107c9a844e576d5c96ce0c86417bf4ef07b6d4
aaron newman <anewman@housetrevethan.com>
2026-08-07 10:45:21 -0500

    Fixed issue calling the incorrect name of the microsoft controller

--- routes/microsoft.php
+++ routes/microsoft.php
@@ -3,8 +3,8 @@
 use Housetrevethan\FilamentAuth\Http\Controllers\MicrosoftLoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('auth/microsoft/redirect', [MicrosoftLoginController::class, 'redirect'])
+Route::get('auth/microsoft/redirect', [MicrosoftLoginController::class, 'create'])
     ->name('auth.microsoft.redirect');
 
-Route::get('auth/microsoft/callback', [MicrosoftLoginController::class, 'callback'])
+Route::get('auth/microsoft/callback', [MicrosoftLoginController::class, 'store'])
     ->name('auth.microsoft.callback');
